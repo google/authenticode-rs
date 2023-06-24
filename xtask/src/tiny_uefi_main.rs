37ea6bd8b4b8de90f46ed3e8a16fd971dfc79bd1
Nicholas Bishop <nbishop@nbishop.net>
2023-06-24 17:27:22 -0400

    xtask: Add tiny_uefi_main.rs
    This will be used to generate a small test file.

---
+++ xtask/src/tiny_uefi_main.rs
@@ -0,0 +1,19 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![no_main]
+#![no_std]
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+fn efi_main() {
+}
