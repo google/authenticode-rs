d046c88ab42b6d8973e34a6135f188d3fa0ef7ec
Nicholas Bishop <nbishop@nbishop.net>
2023-06-24 19:59:30 -0400

    authenticode: Add 32-bit executable test

--- authenticode/tests/test_authenticode.rs
+++ authenticode/tests/test_authenticode.rs
@@ -6,10 +6,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use authenticode::AttributeCertificateIterator;
+use authenticode::{AttributeCertificateIterator, PeTrait};
 use cms::signed_data::SignerIdentifier;
 use digest::{Digest, Update};
-use object::read::pe::PeFile64;
+use object::read::pe::{PeFile32, PeFile64};
 use sha1::Sha1;
 use sha2::Sha256;
 
@@ -26,22 +26,21 @@ impl Update for AuthenticodeHasher {
     }
 }
 
-#[test]
-fn test_authenticode() {
-    let pe = include_bytes!("data/tiny64.signed.efi");
-    let pe = PeFile64::parse(pe.as_slice()).unwrap();
+struct Expected {
+    sha1: &'static str,
+    sha256: &'static str,
+}
+
+fn check_exe(pe: &dyn PeTrait, expected: Expected) {
     let mut hasher = AuthenticodeHasher::default();
-    authenticode::authenticode_digest(&pe, &mut hasher).unwrap();
+    authenticode::authenticode_digest(pe, &mut hasher).unwrap();
     let sha1 = format!("{:x}", hasher.sha1.finalize());
     let sha256 = format!("{:x}", hasher.sha256.finalize());
-    assert_eq!(sha1, "e9bdfb63bdf687b8d3bf144033fcb09d7a393563");
-    assert_eq!(
-        sha256,
-        "a82d7e4f091c44ec75d97746b3461c8ea9151e2313f8e9a4330432ee5f25b2ae"
-    );
+    assert_eq!(sha1, expected.sha1);
+    assert_eq!(sha256, expected.sha256);
 
     // Get the signature.
-    let signatures = AttributeCertificateIterator::new(&pe)
+    let signatures = AttributeCertificateIterator::new(pe)
         .unwrap()
         .unwrap()
         .map(|attr_cert| attr_cert.get_authenticode_signature())
@@ -76,4 +75,24 @@ fn test_authenticode() {
     assert_eq!(cert.tbs_certificate.issuer, sid.issuer);
     assert_eq!(cert.tbs_certificate.subject, sid.issuer);
     assert_eq!(cert.tbs_certificate.serial_number, sid.serial_number);
+}
+
+#[test]
+fn test_authenticode32() {
+    let pe = include_bytes!("data/tiny32.signed.efi");
+    let pe64 = PeFile32::parse(pe.as_slice()).unwrap();
+    check_exe(&pe64, Expected {
+        sha1: "49f239f1cd5083912880e03982bb54528f2c358d",
+        sha256: "4f5b3633fc51d9447beb5c546e9ae6e58d6eb42d1e96d623dc168d97013c08a8",
+    });
+}
+
+#[test]
+fn test_authenticode64() {
+    let pe = include_bytes!("data/tiny64.signed.efi");
+    let pe64 = PeFile64::parse(pe.as_slice()).unwrap();
+    check_exe(&pe64, Expected {
+        sha1: "e9bdfb63bdf687b8d3bf144033fcb09d7a393563",
+        sha256: "a82d7e4f091c44ec75d97746b3461c8ea9151e2313f8e9a4330432ee5f25b2ae",
+    });
 }
