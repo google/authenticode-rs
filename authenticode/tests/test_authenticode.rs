53a1fde9b44e957607d99bc39235e509b37da7e0
Nicholas Bishop <nicholasbishop@google.com>
2023-09-29 16:25:53 -0400

    authenticode: Add a few certificate data corruption tests

--- authenticode/tests/test_authenticode.rs
+++ authenticode/tests/test_authenticode.rs
@@ -8,9 +8,18 @@
 
 #![cfg(feature = "object")]
 
-use authenticode::{AttributeCertificateIterator, PeTrait};
+use authenticode::{
+    AttributeCertificateError, AttributeCertificateIterator, PeTrait,
+};
 use cms::signed_data::SignerIdentifier;
+use core::mem::size_of;
+use core::slice;
 use digest::{Digest, Update};
+use object::endian::LittleEndian as LE;
+use object::pe::{
+    ImageDataDirectory, ImageFileHeader, ImageOptionalHeader64,
+    IMAGE_DIRECTORY_ENTRY_SECURITY,
+};
 use object::read::pe::{PeFile32, PeFile64};
 use sha1::Sha1;
 use sha2::Sha256;
@@ -103,4 +112,109 @@ fn test_authenticode64() {
         sha1: "e9bdfb63bdf687b8d3bf144033fcb09d7a393563",
         sha256: "a82d7e4f091c44ec75d97746b3461c8ea9151e2313f8e9a4330432ee5f25b2ae",
     });
+}
+
+fn modify_image_security_data_dir<F>(f: F) -> Vec<u8>
+where
+    F: FnOnce(&mut ImageDataDirectory),
+{
+    let mut data = include_bytes!("testdata/tiny64.signed.efi").to_vec();
+
+    // Get the offset of the PE header from a fixed offset within the
+    // DOS header.
+    let pe_header_offset = usize::try_from(u32::from_le_bytes(
+        data[0x3c..][..4].try_into().unwrap(),
+    ))
+    .unwrap();
+
+    let start_of_optional_header = pe_header_offset
+        // Skip 4 bytes for the "PE\0\0" signature.
+        + 4
+        // Skip past the rest of the COFF header.
+        + size_of::<ImageFileHeader>();
+
+    let num_data_dirs = usize::try_from(u32::from_le_bytes(
+        data[start_of_optional_header
+             // The field containing the number of data dirs is the last
+             // 4 bytes of the optional header.
+            + size_of::<ImageOptionalHeader64>()
+            - 4..][..4]
+            .try_into()
+            .unwrap(),
+    ))
+    .unwrap();
+
+    let start_of_data_dirs = start_of_optional_header
+        // Skip past the fixed fields of the optional header. The data
+        // directories start immediately after.
+        + size_of::<ImageOptionalHeader64>();
+
+    let data_dirs: &mut [ImageDataDirectory] = unsafe {
+        slice::from_raw_parts_mut(
+            data[start_of_data_dirs..].as_mut_ptr().cast(),
+            num_data_dirs,
+        )
+    };
+
+    f(&mut data_dirs[IMAGE_DIRECTORY_ENTRY_SECURITY]);
+
+    data
+}
+
+/// Test an image with an out-of-bounds size in the security data
+/// directory.
+#[test]
+fn test_cert_table_out_of_bounds() {
+    let data = modify_image_security_data_dir(|data_dir| {
+        // Set a bigger size in the data dir so that it extends past the end
+        // of the image.
+        data_dir.size.set(LE, data_dir.size.get(LE) + 100)
+    });
+
+    let pe = PeFile64::parse(data.as_slice()).unwrap();
+    assert_eq!(
+        AttributeCertificateIterator::new(&pe).unwrap_err(),
+        AttributeCertificateError::OutOfBounds
+    );
+}
+
+/// Test an image with a mismatch between the size of the security data
+/// directory and the actual entries in the table.
+#[test]
+fn test_cert_table_invalid_size() {
+    let data = modify_image_security_data_dir(|data_dir| {
+        // Decrease the size of the table by one, to make its contents
+        // unaligned.
+        data_dir.size.set(LE, data_dir.size.get(LE) - 1)
+    });
+
+    let pe = PeFile64::parse(data.as_slice()).unwrap();
+    assert_eq!(
+        AttributeCertificateIterator::new(&pe).unwrap_err(),
+        AttributeCertificateError::InvalidSize
+    );
+}
+
+/// Test an image containing a certificate with an invalid size.
+#[test]
+fn test_invalid_cert_size() {
+    let mut data = include_bytes!("testdata/tiny64.signed.efi").to_vec();
+    let cert_table_range = {
+        let pe = PeFile64::parse(data.as_slice()).unwrap();
+        pe.certificate_table_range().unwrap().unwrap()
+    };
+    let cert_table = &mut data[cert_table_range];
+    let cert_size: *mut u32 = cert_table.as_mut_ptr().cast();
+    // The cert size must be at least as big as the cert header (8 bytes).
+    unsafe {
+        cert_size.write(7);
+    }
+
+    let pe = PeFile64::parse(data.as_slice()).unwrap();
+    let mut iter = AttributeCertificateIterator::new(&pe).unwrap().unwrap();
+    assert_eq!(
+        iter.next().unwrap().unwrap_err(),
+        AttributeCertificateError::InvalidCertificateSize
+    );
+    assert!(iter.next().is_none());
 }
