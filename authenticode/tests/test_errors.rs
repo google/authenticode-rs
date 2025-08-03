fd7ab588414835392ed03e49a6c1b0bc18f9bb13
Nicholas Bishop <nbishop@nbishop.net>
2025-08-03 13:12:28 -0400

    authenticode: Ignore expected warnings in error tests

--- authenticode/tests/test_errors.rs
+++ authenticode/tests/test_errors.rs
@@ -6,6 +6,9 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// Ignore warnings from not using the return value of `format!`.
+#![expect(unused_must_use)]
+
 use authenticode::{
     AttributeCertificateAuthenticodeError, AttributeCertificateError,
     AuthenticodeSignatureParseError, PeOffsetError,
